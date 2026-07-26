7ed5155ef3f0b9a1aa78424910c9eed22b33a639
drdonuttfanforever-stack <drdonuttfanforever@gmail.com>
2026-07-26 04:13:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/index.hta', (req, res) => {
+app.get('/index.html', (req, res) => {
   res.set('Content-Type', 'application/hta');
-  res.sendFile(__dirname + '/index.hta');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/', (req, res) => {
-  res.redirect('/index.hta');
+  res.redirect('/index.html');
 });
 
 app.listen(port, '0.0.0.0', () => {
