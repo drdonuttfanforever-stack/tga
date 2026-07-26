21b7882e62618eccf47a92a427cd00a0de5e4069
drdonuttfanforever-stack <drdonuttfanforever@gmail.com>
2026-07-26 04:04:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/index.hta', (req, res) => {
-  res.set('Content-Type', 'application/hta');
-  res.sendFile(__dirname + '/index.hta');
-});
-
-app.get('/', (req, res) => {
-  res.redirect('/index.hta');
-});
-
-app.listen(port, '0.0.0.0', () => {
-  console.log('HTA server on port ' + port);
-});
+{
+  "name": "hta-server",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
